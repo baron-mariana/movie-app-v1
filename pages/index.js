4dae472a5407c2f4a273f1574ff298955252b016
baron-mariana <baronmariana28@gmail.com>
2022-04-28 15:17:22 +0300

    changes

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Home() {
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
+            <p>Salut</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
